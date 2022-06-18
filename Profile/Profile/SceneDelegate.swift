13845d32a27eecdac2c53024febdcf3c32c8d162
Алексей Маяков <aleksei@Mac-mini-Aleksej.local>
2022-06-18 17:14:29 +0300

    lector

--- Profile/Profile/SceneDelegate.swift
+++ Profile/Profile/SceneDelegate.swift
@@ -27,7 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
        
             func createProfileViewController() -> UINavigationController {
-            let profileViewController = ProfileViewController()
+            let profileViewController = LogInViewController()
             profileViewController.title = "Profile"
             profileViewController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person.circle"), tag: 1)
             return UINavigationController(rootViewController: profileViewController)
